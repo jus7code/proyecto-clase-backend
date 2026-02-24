20bfd7bd510e643082533230533ada9885a29733
jus7acolombian <95142083+jus7code@users.noreply.github.com>
2026-02-24 14:19:00 -0500

    migraciones de la base de datos

---
+++ database/migrations/2026_02_24_184845_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', length: 100);
+            $table->text('description')->nullable();
+            $table->decimal('price', total: 8, places: 2);
+            $table->timestamps();
+
+            //crear una llave foranea
+            $table->unsignedBigInteger('category_id');
+            $table -> foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
