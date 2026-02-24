5c897ff6f81cd3d9820f008c1005a1958a035212
jus7acolombian <95142083+jus7code@users.noreply.github.com>
2026-02-24 14:52:23 -0500

    creacion seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $tecnologia = new category();
+        $tecnologia -> name ="tecnologia" ;
+        $tecnologia -> description=" Todo lo relacionado a tenologia";
+        $tecnologia -> save();
+
+    }
+}
